4a4431de168338bb220f7bffda006bfd90cdea0f
TheKingsident <kingsident@gmail.com>
2025-08-08 12:21:16 +0100

    feat: add caching configuration and update requirements for Redis support

--- nexus/settings.py
+++ nexus/settings.py
@@ -180,3 +180,14 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+#Cache Config
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': config('REDIS_URL'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        }
+    }
+}
